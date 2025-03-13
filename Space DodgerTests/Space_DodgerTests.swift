e2d60437c6fec2a1eaf2285c9e146b3560a60a21
tradervijeth <151781407+tradervijeth@users.noreply.github.com>
2025-03-13 20:13:26 +0000

    Initial Commit

---
+++ Space DodgerTests/Space_DodgerTests.swift
@@ -0,0 +1,17 @@
+//
+//  Space_DodgerTests.swift
+//  Space DodgerTests
+//
+//  Created by Vithushan Jeyapahan on 13/03/2025.
+//
+
+import Testing
+@testable import Space_Dodger
+
+struct Space_DodgerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
